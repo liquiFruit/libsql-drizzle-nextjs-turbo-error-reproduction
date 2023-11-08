03769c8081807a8060dc392a8624b84fc1420438
John P <johnrprutton@gmail.com>
2023-11-08 11:44:03 +0200

    add nextjs app, db failing "module parse failed"

--- src/packages/db/src/index.ts
+++ src/packages/db/src/index.ts
@@ -2,10 +2,14 @@ import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import { users } from "./schema";
 
+import { join } from "path";
+
 const libsql = createClient({
-  url: "file:local.db",
+  url: "file:" + join(__dirname, "../local.db"),
 });
 
 export const db = drizzle(libsql);
 
 db.select().from(users).then(console.log);
+
+console.log(__dirname);
