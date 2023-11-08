1531d4ac60f0c9498ff3cceb4b6edb9ea180248f
John P <johnrprutton@gmail.com>
2023-11-08 12:48:42 +0200

    publish

--- src/packages/db/src/index.ts
+++ src/packages/db/src/index.ts
@@ -3,6 +3,7 @@ import { drizzle } from "drizzle-orm/libsql";
 import { users } from "./schema";
 
 import { join } from "path";
+import { sql } from "drizzle-orm";
 
 const libsql = createClient({
   url: "file:" + join(__dirname, "../local.db"),
@@ -10,6 +11,13 @@ const libsql = createClient({
 
 export const db = drizzle(libsql);
 
-db.select().from(users).then(console.log);
+export async function getUsers() {
+  console.log(
+    await db.run(sql`
+    SELECT name FROM sqlite_master WHERE type='table';
+  `)
+  );
+  return await db.select().from(users);
+}
 
-console.log(__dirname);
+getUsers().then(console.log);
